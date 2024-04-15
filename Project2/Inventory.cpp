12cab7ab8ccf3ed211476126b9dfb4293deeb7dd
Kim Nguyen <110859844+kpn3799@users.noreply.github.com>
2024-04-15 11:36:30 -0500

    Update Inventory.cpp
    Commentary for inventory

--- Project2/Inventory.cpp
+++ Project2/Inventory.cpp
@@ -5,31 +5,32 @@
 
 using namespace std;
 
-class Inventory
+class Inventory // this class will display our entire inventory
 {
 private:
-    struct Product {
+    
+    struct Product { // make the attributes for our product
         string productName;
         int productID;
         double productPrice;
         int productQuantity;
     };
 
-    vector<Product> productList;
+    vector<Product> productList; // create a vector called 'productList' using the variables (attributes) from our Product Struct. Declare vector in Inventory class b/c it's easier to use within the same class.
 
 public:
-    void addProduct(const string& name, int id, double price, int quantity)
+    void addProduct(const string& name, int id, double price, int quantity) // function to add product: allows struct variables to equal the variables we were asked to enter from main(). 
     {
-        Product newProduct;
-        newProduct.productName = name;
+        Product newProduct; // we have to make an object from Product struct to put these products somewhere. make new object called newProduct.
+        newProduct.productName = name; // the productName of newProduct object (in this Inventory clasS) is going to equal the name passed through from the main(). 
         newProduct.productID = id;
         newProduct.productPrice = price;
         newProduct.productQuantity = quantity;
-        productList.push_back(newProduct);
+        productList.push_back(newProduct); // after we set struct variables equal to parameter, use the vector function to push_back the newProduct object into the vector.
         cout << "Product added successfully." << endl;
     }
-
-    void displayInventory() const
+ 
+    void displayInventory() const // function to display entire inventory. no variables to be changed so it is constant.
     {
         cout << "\nInventory Details:\n";
         if (productList.empty()) {
@@ -39,13 +40,13 @@ public:
             cout << setw(15) << "Product Name" << setw(15) << "Product ID" << setw(15) << "Price" << setw(15) << "Quantity" << endl;
             for (const auto& product : productList) {
                 cout << setw(15) << product.productName << setw(15) << product.productID << setw(15) << "$" << product.productPrice << setw(15) << product.productQuantity << endl;
-            }
+            }                    // ????????????
         }
     }
 
-    void displayProductDetails(int id) const
-    {
-        for (const auto& product : productList) {
+    void displayProductDetails(int id) const // function to display a specific product: const, b/c no variable should be changed in this function
+    {                                          // the only thing we need to pass through is the id, to specify which product ID we want to display.
+        for (const auto& product : productList) { // we just made a product variable. auto is put here to automatically match to the type. this is a loop will basically iterate over each element of the vector productList.
             if (product.productID == id) {
                 cout << "\nProduct Details:\n";
                 cout << "Product Name: " << product.productName << endl;

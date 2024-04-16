c4d758ecca7ad2fafa8f01c26e647dbe98327afd
Kim Nguyen <110859844+kpn3799@users.noreply.github.com>
2024-04-16 10:59:46 -0500

    Update Inventory.cpp

--- Project2/Inventory.cpp
+++ Project2/Inventory.cpp
@@ -9,7 +9,7 @@ class Inventory // this class will display our entire inventory
 {
 private:
     
-    struct Product { // make the attributes for our product
+    struct Product { // make the attributes we want to include for our product
         string productName;
         int productID;
         double productPrice;
@@ -21,33 +21,33 @@ private:
 public:
     void addProduct(const string& name, int id, double price, int quantity) // function to add product: allows struct variables to equal the variables we were asked to enter from main(). 
     {
-        Product newProduct; // we have to make an object from Product struct to put these products somewhere. make new object called newProduct.
+        Product newProduct; // we have to make a new object from Product struct. This means that each object will be able to use the variables from this struct. Each object of this will be put into our vector.
         newProduct.productName = name; // the productName of newProduct object (in this Inventory clasS) is going to equal the name passed through from the main(). 
         newProduct.productID = id;
         newProduct.productPrice = price;
         newProduct.productQuantity = quantity;
-        productList.push_back(newProduct); // after we set struct variables equal to parameter, use the vector function to push_back the newProduct object into the vector.
+        productList.push_back(newProduct); // after we set struct variables equal to parameter, the data that is stored in newProduct will be put into our vector productList.
         cout << "Product added successfully." << endl;
     }
  
     void displayInventory() const // function to display entire inventory. no variables to be changed so it is constant.
     {
         cout << "\nInventory Details:\n";
-        if (productList.empty()) {
+        if (productList.empty()) { // if the vector has 0 objects, then it is empty.
             cout << setw(50) << "Inventory is empty." << endl;
         }
         else {
             cout << setw(15) << "Product Name" << setw(15) << "Product ID" << setw(15) << "Price" << setw(15) << "Quantity" << endl;
-            for (const auto& product : productList) {
+            for (const auto& product : productList) { // this is a for loop that will iterate each element in the vector productList. The 'auto' is made to detect the type below: 
                 cout << setw(15) << product.productName << setw(15) << product.productID << setw(15) << "$" << product.productPrice << setw(15) << product.productQuantity << endl;
-            }                    // ????????????
+            }                    
         }
     }
 
     void displayProductDetails(int id) const // function to display a specific product: const, b/c no variable should be changed in this function
     {                                          // the only thing we need to pass through is the id, to specify which product ID we want to display.
-        for (const auto& product : productList) { // we just made a product variable. auto is put here to automatically match to the type. this is a loop will basically iterate over each element of the vector productList.
-            if (product.productID == id) {
+        for (const auto& product : productList) { // make a product variable. auto is put here to automatically match to the type. this is a loop will basically iterate over each element of the vector productList.
+            if (product.productID == id) { 
                 cout << "\nProduct Details:\n";
                 cout << "Product Name: " << product.productName << endl;
                 cout << "Product ID: " << product.productID << endl;
@@ -59,9 +59,9 @@ public:
         cout << "Product with ID " << id << " not found." << endl;
     }
 
-    void deleteProduct(int id)
+    void deleteProduct(int id) 
     {
-        for (auto it = productList.begin(); it != productList.end(); ++it)
+        for (auto it = productList.begin(); it != productList.end(); ++it) 
         {
             if (it->productID == id)
             {

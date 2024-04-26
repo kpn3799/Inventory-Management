9c92a02d55e489778328cb95bb27c9b625ece5a6
Kim Nguyen <110859844+kpn3799@users.noreply.github.com>
2024-04-26 11:19:04 -0500

    Update Main.cpp

--- Project2/Main.cpp
+++ Project2/Main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 int main()
 {
-    Inventory inventory;
+    Inventory inventory; // Create an object inventory from our class called 'Inventory'
     int choice;
         cout << "Welcome to Inventory Manager! Please choose an option:";
-        std::cout << "\n" << std::setfill('~') << std::setw(100);
+        std::cout << "\n" << std::setfill('~') << std::setw(100); // Make it look nice w/ iomanip
         cout << "\n1. Register a product ID\n";
         cout << "2. Add product\n";
         cout << "3. Display inventory\n";
@@ -18,19 +18,18 @@ int main()
         cout << "5. Delete a product\n";
         cout << "6. Exit";
         std::cout << "\n" << std::setfill('~') << std::setw(100);
-    do {
+    do { // this 'do' loop will
         cout << "\nEnter your choice from the top menu:\n";
-        cin >> choice;
+        cin >> choice; 
 
         switch (choice) {
-        case 1: {
+        case 1: { // if user picks choice 1, then ask user to enter id (to register the ID first w/o details to indicate that the product is existing)
             int id;
             cout << "\nEnter Product ID Number:\n";
             cin >> id;
-            // You can register product ID here if needed
             break;
         }
-        case 2: {
+        case 2: { // if user picks choice 2, then ask user to enter product id, quantity, price
             string name;
             int id, quantity;
             double price;
@@ -43,35 +42,35 @@ int main()
             cin >> price;
             cout << "\nEnter Product Quantity: ";
             cin >> quantity;
-            inventory.addProduct(name, id, price, quantity);
+            inventory.addProduct(name, id, price, quantity); // now that we've input product details, call the addProduct function to our inventory
             break;
         }
-        case 3:
+        case 3: // if user picks choice 3, call the displayInventory function. this is a void, so values will be returned to the main()
             inventory.displayInventory();
             break;
-        case 4: {
+        case 4: { // if the user picks choice 4, it will display the individual product details.
             int id;
             cout << "\nEnter the product ID to display details: ";
             cin >> id;
-            inventory.displayProductDetails(id);
+            inventory.displayProductDetails(id); // call to the function displayProductDetails that will pass id through the user input id. 
             break;
         }
-        case 5: {
+        case 5: { // calls the deleteProduct function that passes the id as a parameter.
             int id;
             cout << "\nEnter the product ID to delete: ";
             cin >> id;
             inventory.deleteProduct(id);
             break;
         }
-        case 6: // 6 is the exit. the switch method will continue if the user doesn't enter 6, it will ask to enter a choice.
+        case 6: // 6 will exit the menu program
             cout << "\nThank you!";
             break;
-        default:
+        default: // if 'choice' does not match the above switch case, then this error message will display.
             cout << "\nError.. enter a number between 1 and 6. ";
             break;
         }
 
-    } while (choice != 6);
+    } while (choice != 6); // this 'do while' loop will keep asking user to input choice UNTIL the user inputs '6', then the loop will end and forego the program.
 
     return 0;
 }
